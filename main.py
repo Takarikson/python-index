3d9d628b5b52af98640c0164a352ef4d1526bebe
Krzysztof Fila <kfila17@gmail.com>
2021-02-08 16:09:17 +0000

    elementy w indeksie na parzyste i nieparzyste

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 # liczby w indeksie
-for a in [1,2,3,]:
+a = [1,2,3,4,5,6,7,8,9,10]
+for a in a:
     c = a-1
     print(c)
+
+tab = [1,2,3,4,5,6,7,8,9,10]
+print(tab[::2]) # co drugi element począwszy od 0-go elem. w indeksie (nieparzyste) == tab[0::2]
+print(tab[1::2]) # co drugi element począwszy od 1-go elem. w indeksie (nieparzyste) == tab[0::2]
+print(tab[2:-2:2]) #hmm
